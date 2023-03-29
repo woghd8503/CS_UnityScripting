4b6b52f4900261b0118ca8eb87f284974d70b52e
James <woghd8503@naver.com>
2023-03-29 11:00:00 +0900

    Enum

---
+++ UnityScripting/Assets/Scripts/MyScriptFile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScriptFile : MonoBehaviour
+{
+    // 열거형을 사용해 가능한 상태를 정의한다
+    public enum EnemyState { CHASE, FLEE, FIGHT, HIDE };
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
