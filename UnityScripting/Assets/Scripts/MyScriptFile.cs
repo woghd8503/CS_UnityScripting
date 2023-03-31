348a00dafa03d4fbdf0777e15a9bf5f6d935d49f
James <woghd8503@naver.com>
2023-03-31 11:00:00 +0900

    Enum

--- UnityScripting/Assets/Scripts/MyScriptFile.cs
+++ UnityScripting/Assets/Scripts/MyScriptFile.cs
@@ -6,6 +6,12 @@ public class MyScriptFile : MonoBehaviour
 {
     // 열거형을 사용해 가능한 상태를 정의한다
     public enum EnemyState { CHASE, FLEE, FIGHT, HIDE };
+
+    // 적의 현재 상태
+    public EnemyState ActiveState = EnemyState.CHASE;
+
+    // 여기에서 초기화한다
+
     void Start()
     {
         
