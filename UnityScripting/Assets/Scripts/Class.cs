65f4fc0c130ca738cc63e3df876372e1e2bac994
James <woghd8503@naver.com>
2023-04-18 13:42:48 +0900

    Class

---
+++ UnityScripting/Assets/Scripts/Class.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class : MonoBehaviour
+{
+    // 오크의 트랜스폼 컴포넌트 (위치, 회전, 크기)에 대한 참조변수다
+    private Transform ThisTransform = null;
+
+    // 오크 상태의 열거형
+    public enum OrcStates { NETURAL, ATTACK_MELEE, ATTACK_RANGE }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
