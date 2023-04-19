7eb9ba3eabd865c248eecc8bfd52bdee62268684
James <woghd8503@naver.com>
2023-04-19 13:22:01 +0900

    Class

--- UnityScripting/Assets/Scripts/Class.cs
+++ UnityScripting/Assets/Scripts/Class.cs
@@ -10,15 +10,38 @@ public class Class : MonoBehaviour
     // 오크 상태의 열거형
     public enum OrcStates { NETURAL, ATTACK_MELEE, ATTACK_RANGE }
 
-    // Start is called before the first frame update
+    // 오크의 현재 상태
+    public OrcStates CurrentState = OrcStates.NETURAL;
+
+    // 오크가 초당 이동 속도(m/s)
+    public float OrcSpeed = 10.0f;
+
+    // 오크가 클레이어에게 호의적인지
+    public bool isFriendly = false;
+
+    // ----------------------------------------------------------------------
+    // 여기에서 초기화한다
     void Start()
     {
-        
+        // 오크의 트랜스폼을 얻는다
+        ThisTransform = transform;
     }
-
-    // Update is called once per frame
+    // ----------------------------------------------------------------------
+    // Update는 매 프레임마다 한 번씩 호출된다
     void Update()
     {
         
+    }
+    // ----------------------------------------------------------------------
+    // 오크의 상태별 동작들
+    public void AttackMelee()
+    {
+        // 여기에서 근접 공격을 한다
+    }
+    // ----------------------------------------------------------------------
+    public void AttackRange()
+    {
+        // 여기에서 원거리 공격을 한다
+
     }
 }
