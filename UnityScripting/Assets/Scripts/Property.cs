72608ef3403c30d310bef50ed4251f92c0abbe4b
James <woghd8503@naver.com>
2023-04-30 00:55:39 +0900

    Property

---
+++ UnityScripting/Assets/Scripts/Property.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Property : MonoBehaviour
+{
+    // --------------------------------------------
+    // 오브젝트에 컴포넌트로 붙일 수 있는 예제 클래스
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
