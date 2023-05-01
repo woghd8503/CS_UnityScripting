d234d4bf74f88f4bfa1621a898ce1df93a04aacf
JamesPark <entrepreneurjames@JamesParks-MacBook-Pro.local>
2023-05-01 23:15:45 +0900

    Annotation

---
+++ UnityScripting/Assets/Scripts/Annotation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Annotation : MonoBehaviour
+{
+    
+    int AddNumbers(int Num, int Num2)
+    {
+        return Num1 + Num2;
+    }
+}
