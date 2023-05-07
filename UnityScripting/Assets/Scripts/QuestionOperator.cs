7ea28e390a79c3f4564a07544b4ff680a7deec7e
James <woghd8503@naver.com>
2023-05-07 15:58:24 +0900

    QestionOperator

--- UnityScripting/Assets/Scripts/QuestionOperator.cs
+++ UnityScripting/Assets/Scripts/QuestionOperator.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class QuestionOperator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Operator()
     {
-        // 이 오브젝트에 붙어있는 모든 컴포넌트/스크립트에서 MyFunction 함수를 부른다
-        SendMessage("MyFunction", SendMessageOptions.DontRequireReceiver);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int i = 2 < 3 ? 5 : 6;
     }
 }
