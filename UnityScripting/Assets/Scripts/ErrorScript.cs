559052f591ecd44026e0e24be555c32d1e48b3a5
James <woghd8503@naver.com>
2023-05-12 11:00:00 +0900

    ErrorScript

--- UnityScripting/Assets/Scripts/ErrorScript.cs
+++ UnityScripting/Assets/Scripts/ErrorScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ErrorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int MyNumber = 5;
+
+    // 여기에서 초기화한다
     void Start()
     {
-        
+        Debug.Log("Object created in scene at position: " + transform.position.ToString());
+        MyNumber = 7;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 에러 발생
+        //Mynumber = 10;
     }
 }
