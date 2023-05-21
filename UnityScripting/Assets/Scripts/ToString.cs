675b1bdb34c0e810b3b7acff3de2a0d97b73df77
James <woghd8503@naver.com>
2023-05-21 22:11:36 +0900

    ToString

---
+++ UnityScripting/Assets/Scripts/ToString.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//----------------------------------------------
+// 예제 적 오우거 클래스
+
+public class ToString : MonoBehaviour
+{
+    public enum AttackType { PUNCH, MAGIC, SWORD, SPEAR };
+
+    // 현재 사용되는 공격 형태
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
